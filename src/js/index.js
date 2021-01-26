0d8a542c1e0e3a74ef2853f616db5312a0af5cb6
orchlon <ch.orchlon@gmai.com>
2021-01-26 22:16:29 +0800

    search button hiiw

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,39 @@
 require("@babel/polyfill");
 import Search from "./model/search";
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from './view/searchView';
+/**
+ * web app tuluw
+ * hailtiin query, vr dun
+ * tuhain vzvvlj bga jor
+ * zahialj bgaa joriin nairlaguud
+ */
 
-let search = new Search("pasta");
-search.doSearch().then(r => console.log(r));
+const state = {};
+
+const controlSearch = async () => {
+    // 1) webees hailtiin tvlhvvr vgiig gargaj awna
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2) great an object that do search
+        state.search = new Search(query);
+
+        // 3) prepare UI for searchin
+        searchView.clearSearchQuery();
+        searchView.clearSearchResult();
+        renderLoader(elements.searchResultDiv);
+
+        // 4) do search
+        await state.search.doSearch();
+        clearLoader();
+
+        // 5) show result from search
+        searchView.renderRecipes(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+    controlSearch();
+});
